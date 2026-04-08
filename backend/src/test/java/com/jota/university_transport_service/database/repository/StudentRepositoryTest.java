eeff7db90e02472d782917e73eda6d71e200b7a8
JoaoPedro53 <jpsv32@gmail.com>
2026-04-08 14:45:37 -0300

    refactor: I refactored tests and added new tests, for the method: listAllStudentsByBussId

--- backend/src/test/java/com/jota/university_transport_service/database/repository/StudentRepositoryTest.java
+++ backend/src/test/java/com/jota/university_transport_service/database/repository/StudentRepositoryTest.java
@@ -1,9 +1,6 @@
 package com.jota.university_transport_service.database.repository;
 
-import com.jota.university_transport_service.database.model.Address;
-import com.jota.university_transport_service.database.model.Course;
-import com.jota.university_transport_service.database.model.Institution;
-import com.jota.university_transport_service.database.model.Student;
+import com.jota.university_transport_service.database.model.*;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -16,6 +13,7 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.ThreadLocalRandom;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -30,16 +28,20 @@ class StudentRepositoryTest {
 
     @BeforeEach
     void init(){
+        var routerBuss = RouterBuss.builder().name("Fernandes Lima").build();
         var adress = Address.builder().addressHouse("Frei Fernando").cep("5777-0000").houseNumber("22").city("Cajueiro")
                 .state("Alagoas").build();
+        var driver = Driver.builder().id(ThreadLocalRandom.current().nextLong(100_000)).name("João Pedro").age(19).phoneNumber("82 998704350").cpf("121253013-90")
+                .address(adress).build();
         var course = Course.builder().name("Bacharel em SI").startDate(LocalDate.of(2025, 9, 10))
                 .endDate(LocalDate.of(2029, 9, 10)).build();
         var institution = Institution.builder().name("IFAL").build();
+        var buss = Buss.builder().id(ThreadLocalRandom.current().nextLong(100_000)).bussSeats("42").color("Azul").plate("A654-87A").routerBuss(routerBuss).driver(driver).build();
 
         var student1 = Student.builder().id(1L).name("João Pedro").age(19).phoneNumber("82 996704350").cpf("121253013-90").registration("82726X-AL1")
-                .address(adress).course(course).institution(institution).build();
-        var student2 = Student.builder().id(1L).name("Alfredo").age(19).phoneNumber("82 996704350").cpf("121873013-90").registration("82726X-AL1")
-                .address(adress).course(course).institution(institution).build();
+                .address(adress).course(course).institution(institution).buss(buss).build();
+        var student2 = Student.builder().id(2L).name("Alfredo").age(19).phoneNumber("82 996704350").cpf("121873013-90").registration("82726X-AL1")
+                .address(adress).course(course).institution(institution).buss(buss).build();
         studentList.addAll(List.of(student1, student2));
     }
 
@@ -48,7 +50,7 @@ class StudentRepositoryTest {
     @DisplayName("listAll return student list when successful")
     void listAll_ReturnListStudents_WhenSuccessful(){
         BDDMockito.when(studentData.getSTUDENTS()).thenReturn(studentList);
-        Assertions.assertThat(repository.listAll()).isNotNull().hasSameElementsAs(studentList);
+        Assertions.assertThat(repository.listAll()).hasSameElementsAs(studentList);
     }
 
     @Test
@@ -59,7 +61,7 @@ class StudentRepositoryTest {
 
         var studentExpected = studentList.getFirst();
         var studentsByName = repository.findByName(studentExpected.getName());
-        Assertions.assertThat(studentsByName).isNotNull().contains(studentExpected);
+        Assertions.assertThat(studentsByName).contains(studentExpected);
     }
 
     @Test
@@ -69,7 +71,7 @@ class StudentRepositoryTest {
         BDDMockito.when(studentData.getSTUDENTS()).thenReturn(studentList);
 
         var listStudentEmpty = repository.findByName(null);
-        Assertions.assertThat(listStudentEmpty).isNotNull().isEmpty();
+        Assertions.assertThat(listStudentEmpty).isEmpty();
     }
 
     @Test
@@ -81,7 +83,7 @@ class StudentRepositoryTest {
         var studentExpected = studentList.getFirst();
         var studentById = repository.findById(studentExpected.getId());
 
-        Assertions.assertThat(studentById).isNotNull().contains(studentExpected);
+        Assertions.assertThat(studentById).contains(studentExpected);
     }
 
     @Test
@@ -91,6 +93,27 @@ class StudentRepositoryTest {
         BDDMockito.when(studentData.getSTUDENTS()).thenReturn(studentList);
 
         var studentById = repository.findById(100L);
-        Assertions.assertThat(studentById).isNotNull().isEmpty();
+        Assertions.assertThat(studentById).isEmpty();
+    }
+
+    @Test
+    @Order(5)
+    @DisplayName("listAllStudentsByBussId return students list with same buss give when buss is found")
+    void listAllStudentsByBussId_ReturnStudentsListWithSameBuss_WhenSuccessful(){
+        BDDMockito.when(studentData.getSTUDENTS()).thenReturn(studentList);
+
+        var idBuss = studentList.getFirst().getBuss().getId();
+        var studentsByBuss = repository.listAllStudentsByBussId(idBuss);
+        Assertions.assertThat(studentsByBuss).hasSameElementsAs(studentList);
+    }
+
+    @Test
+    @Order(5)
+    @DisplayName("listAllStudentsByBussId return students empty list when give buss not found")
+    void listAllStudentsByBussId_ReturnStudentsEmptyListWhenBussIsNotFound_WhenSuccessful(){
+        BDDMockito.when(studentData.getSTUDENTS()).thenReturn(studentList);
+
+        var studentsByBuss = repository.listAllStudentsByBussId(100L);
+        Assertions.assertThat(studentsByBuss).isEmpty();
     }
 }
