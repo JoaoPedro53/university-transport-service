8d22dc02ed81984b3686c8b936f0623cd0ceddf3
JoaoPedro53 <jpsv32@gmail.com>
2026-04-09 12:40:31 -0300

    feat: added Equals&HashCode for Student class, and new tests in StudentService

--- backend/src/main/java/com/jota/university_transport_service/database/model/Student.java
+++ backend/src/main/java/com/jota/university_transport_service/database/model/Student.java
@@ -7,6 +7,7 @@ import lombok.*;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@EqualsAndHashCode
 public class Student {
     private Long id;
     private String name;
--- backend/src/test/java/com/jota/university_transport_service/service/StudentServiceTest.java
+++ backend/src/test/java/com/jota/university_transport_service/service/StudentServiceTest.java
@@ -1,9 +1,6 @@
 package com.jota.university_transport_service.service;
 
-import com.jota.university_transport_service.database.model.Address;
-import com.jota.university_transport_service.database.model.Course;
-import com.jota.university_transport_service.database.model.Institution;
-import com.jota.university_transport_service.database.model.Student;
+import com.jota.university_transport_service.database.model.*;
 import com.jota.university_transport_service.database.repository.StudentRepository;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.*;
@@ -12,10 +9,13 @@ import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.ThreadLocalRandom;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -30,16 +30,20 @@ class StudentServiceTest {
 
     @BeforeEach
     void init(){
+        var routerBuss = RouterBuss.builder().name("Fernandes Lima").build();
         var adress = Address.builder().addressHouse("Frei Fernando").cep("5777-0000").houseNumber("22").city("Cajueiro")
                 .state("Alagoas").build();
+        var driver = Driver.builder().id(ThreadLocalRandom.current().nextLong(100_000)).name("João Pedro").age(19).phoneNumber("82 998704350").cpf("121253013-90")
+                .address(adress).build();
         var course = Course.builder().name("Bacharel em SI").startDate(LocalDate.of(2025, 9, 10))
                 .endDate(LocalDate.of(2029, 9, 10)).build();
         var institution = Institution.builder().name("IFAL").build();
+        var buss = Buss.builder().id(ThreadLocalRandom.current().nextLong(100_000)).bussSeats("42").color("Azul").plate("A654-87A").routerBuss(routerBuss).driver(driver).build();
 
         var student1 = Student.builder().id(1L).name("João Pedro").age(19).phoneNumber("82 996704350").cpf("121253013-90").registration("82726X-AL1")
-                .address(adress).course(course).institution(institution).build();
-        var student2 = Student.builder().id(1L).name("Alfredo").age(19).phoneNumber("82 996704350").cpf("121873013-90").registration("82726X-AL1")
-                .address(adress).course(course).institution(institution).build();
+                .address(adress).course(course).institution(institution).buss(buss).build();
+        var student2 = Student.builder().id(2L).name("Alfredo").age(19).phoneNumber("82 996704350").cpf("121873013-90").registration("82726X-AL1")
+                .address(adress).course(course).institution(institution).buss(buss).build();
         studentList.addAll(List.of(student1, student2));
     }
 
@@ -72,6 +76,51 @@ class StudentServiceTest {
 
         var emptyStudentsList = service.findAll("x");
         Assertions.assertThat(emptyStudentsList).isEmpty();
+    }
+
+    @Test
+    @Order(4)
+    @DisplayName("findById return student with give id")
+    void findAll_ReturnStudentWithGiveId_WhenSuccessful(){
+        var studentExpected = studentList.getFirst();
+        BDDMockito.when(repository.findById(studentExpected.getId())).thenReturn(Optional.of(studentExpected));
+
+        var studentById = service.findByIdOrElseThrow(studentExpected.getId());
+        Assertions.assertThat(studentExpected).isEqualTo(studentById);
+    }
+
+    @Test
+    @Order(5)
+    @DisplayName("findById throw ResponseStatusException when id is not found")
+    void findAll_ThrowResponseStatusException_WhenSuccessful(){
+        var student = studentList.getFirst();
+        BDDMockito.when(repository.findById(student.getId())).thenReturn(Optional.empty());
+
+        Assertions.assertThatException()
+                .isThrownBy(() -> service.findByIdOrElseThrow(student.getId()))
+                .isInstanceOf(ResponseStatusException.class);
+    }
+
+    @Test
+    @Order(6)
+    @DisplayName("listAllStudentsByBussId return student list where all have the same buss")
+    void findAll_ReturnStudentsListHaveTheSameBuss_WhenSuccessful(){
+        var student = studentList.getFirst();
+        BDDMockito.when(repository.listAllStudentsByBussId(student.getBuss().getId())).thenReturn(studentList);
+
+        var studentsByBuss = service.listAllStudentsByBussId(student.getBuss().getId());
+        Assertions.assertThat(studentsByBuss).contains(student);
+    }
+
+    @Test
+    @Order(7)
+    @DisplayName("listAllStudentsByBussId return student empty list when buss is not found")
+    void findAll_ReturnStudentsEmptyListWhenBussIsNotFound_WhenSuccessful(){
+        var student = studentList.getFirst();
+        BDDMockito.when(repository.listAllStudentsByBussId(student.getBuss().getId())).thenReturn(List.of());
+
+        var studentsByBuss = service.listAllStudentsByBussId(student.getBuss().getId());
+        Assertions.assertThat(studentsByBuss).isEmpty();
     }
 
 
